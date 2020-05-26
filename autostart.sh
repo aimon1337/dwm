b594b4036be6b146912368d8f47d86093e5fd977
Andrei Corbu <corbu.andrei@yahoo.com>
2020-05-26 17:38:43 +0300

    first commit

---
+++ autostart.sh
@@ -0,0 +1,12 @@
+# layouts
+dwmc viewex 2 && dwmc setlayoutex 1
+dwmc viewex 3 && dwmc setlayoutex 1
+dwmc viewex 0
+# programs
+udiskie &
+/usr/bin/dunst &
+discord &
+xrdb ~/etc/Xresources
+xsetroot -cursor_name arrow
+udiskie &
+feh --no-fehbg --bg-fill ~/etc/wall2.jpg
